35125d33ce0e5dc843d04b1590d9307875860d86
Rahithyateja <108253538+Rahithyateja07@users.noreply.github.com>
2023-01-20 18:40:57 +0530

    changes required

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import NavBar from './components/NavBar';
+import News from './components/News';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+
+export default class App extends Component {
+
+  apiKey = "b55748f6ced94bc69d34925ea68b8734"
+  
+
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+       
+          <NavBar />
+          
+          <Routes>
+            <Route exact path='/' element={<News   apiKey={this.apiKey} key="general" pageSize={5} country="us" category="general" />}></Route>
+            <Route exact path='/business' element={<News  apiKey={this.apiKey} key="business" pageSize={5} country="us" category="business" />}></Route>
+            <Route exact path='/entertainment' element={<News   apiKey={this.apiKey} key="entertainment" pageSize={5} country="us" category="entertainment" />}></Route>
+            <Route exact path='/general ' element={<News   apiKey={this.apiKey} key="general" pageSize={5} country="us" category="general" />}></Route>
+            <Route exact path='/health' element={<News   apiKey={this.apiKey} key="health" pageSize={5} country="us" category="health" />}></Route>
+            <Route exact path='/science' element={<News   apiKey={this.apiKey} key="science" pageSize={5} country="us" category="science" />}></Route>
+            <Route exact path='/sports' element={<News   apiKey={this.apiKey} key="sports" pageSize={5} country="us" category="sports" />}></Route>
+            <Route exact path='/technology' element={<News   apiKey={this.apiKey} key="technology" pageSize={5} country="us" category="technology" />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    )
+  }
 }
 
-export default App;
+//b55748f6ced94bc69d34925ea68b8734
+
+
